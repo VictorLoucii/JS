0240be7594091e47c1b8aa1312589e9508a8daeb
VictorLoucii <147796940+VictorLoucii@users.noreply.github.com>
2025-01-11 04:17:45 +0000

    comparsion0fDatatypes

---
+++ basics/05DatatypesComparision.js
@@ -0,0 +1,27 @@
+// console.log(2<1)  //output: false
+// console.log(1<=2)       //output: true
+// console.log(2!=1)   //output: true
+// console.log(2==1)   //output: false
+
+// console.log("2">1)  //output: true
+// console.log("02">1)  //output: true
+
+console.log(null>0)  //output: false
+console.log(null==0)  //output: false
+console.log(null>=0)  //output: true
+console.log(null<0)  //output: false
+console.log(null==undefined)  //output: true
+
+//strict equality i.e ===
+// console.log(null==undefined)  //output: false
+// console.log(2==="2")  //output: false
+
+console.log("5" + 2); // "52" (2 is converted to a string)
+console.log("5" + 2 + 4); // "524" (2 and 4 are converted to a strings)
+
+
+
+
+
+
+
